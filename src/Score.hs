4ef90f6d49b7d57967a457e5c8649de7b4f4e614
Bas Biezemans <bas.biezemans@gmail.com>
2022-02-26 08:26:10 +0800

    Refactor: combine functions into module

---
+++ src/Score.hs
@@ -0,0 +1,27 @@
+module Score where
+
+import Data.Bifunctor (bimap)
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
+import Game
+
+type Score = (CodeMaker, CodeBreaker)
+
+init :: Score
+init = (CodeMaker 0, CodeBreaker 0)
+
+toString :: Score -> String
+toString score = show ( codeMaker score
+                      , codeBreaker score )
+
+fromString :: String -> Score
+fromString = bimap CodeMaker CodeBreaker . scoreVals
+    where
+        scoreVals = fromMaybe (0,0) . safeRead
+        safeRead s = readMaybe s :: Maybe (Int, Int)
+
+codeMaker :: Score -> Int
+codeMaker = getMakerPoints . fst
+
+codeBreaker :: Score -> Int
+codeBreaker = getBreakerPoints . snd
