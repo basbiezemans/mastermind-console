41b471d25a98092bddd6ff2fa0fa5c3a9f118308
Bas Biezemans <bas.biezemans@gmail.com>
2022-04-11 09:02:45 +0800

    Refactor: split phase

--- src/Hint.hs
+++ src/Hint.hs
@@ -1,4 +1,7 @@
-module Hint where
+module Hint
+    ( Hint
+    , makeHint
+    ) where
 
 import Data.List (intersperse, intersect)
 import Data.Char (intToDigit)
@@ -11,13 +14,22 @@ instance Show Hint where
     show (Hint xs) = intersperse ',' $ map intToDigit xs
 
 -- | Take a secret code and a guess, and return a hint which
--- shows how many digits match and/or are included in the guess.
+-- shows how many digits are correct and/or present in the guess.
 makeHint :: Code -> Guess -> Hint
 makeHint code guess = Hint (ones ++ zeros)
   where
-    pairs = zip (toList code) (toList $ unGuess guess)
-    pair = unzip $ filter (uncurry (/=)) pairs
-    n = length (toList code) - length (fst pair)
-    ones = replicate n 1
-    m = length $ uncurry intersect pair
-    zeros = replicate m 0
+    pairs = listOfPairs code guess
+    ones = replicate (numCorrect pairs) 1
+    zeros = replicate (numPresent $ unequal pairs) 0
+
+numCorrect :: Eq a => [(a, a)] -> Int
+numCorrect pairs = length pairs - length (unequal pairs)
+
+numPresent :: Eq a => [(a, a)] -> Int
+numPresent = length . uncurry intersect . unzip
+
+unequal :: Eq a => [(a, a)] -> [(a, a)]
+unequal = filter (uncurry (/=))
+
+listOfPairs :: Code -> Guess -> [(Int, Int)]
+listOfPairs code guess = zip (toList code) (toList $ unGuess guess)
