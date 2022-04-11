3f2cb284ab6f232f64b89c510916e79035744a11
Bas Biezemans <bas.biezemans@gmail.com>
2022-04-11 10:44:19 +0800

    Add function: both, blackbird

--- src/Utils.hs
+++ src/Utils.hs
@@ -4,3 +4,14 @@ module Utils where
 ensure :: (a -> Bool) -> a -> Maybe a
 ensure p v | p v       = Just v
            | otherwise = Nothing
+
+-- | Apply a single function to both components of a pair
+both :: (a -> b) -> (a, a) -> (b, b)
+both f (x, y) = (f x, f y)
+
+-- | B1 combinator: λabcd.a(bcd)
+(.:), blackbird :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(.:)      = blackbird
+blackbird = (.) . (.)
+
+infixr 8 .:
