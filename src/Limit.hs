0742f5947efc8a70a491db46b7130fc020f88b1a
Bas Biezemans <bas.biezemans@gmail.com>
2022-04-11 08:52:43 +0800

    Refactor: combine functions

--- src/Limit.hs
+++ src/Limit.hs
@@ -19,8 +19,5 @@ limitOr :: Int -> Maybe Int -> Limit
 limitOr _  (Just x) = Limit x
 limitOr def Nothing = Limit def
 
-safeRead :: String -> Maybe Int
-safeRead s = readMaybe s :: Maybe Int
-
 safeValue :: String -> Maybe Int
-safeValue s = safeRead s >>= ensure (`elem` [8..12])
+safeValue s = readMaybe s >>= ensure (`elem` [8..12])
