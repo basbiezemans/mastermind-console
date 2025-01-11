a69aeb01a9db9f6fe333f962422aab235aa72218
Bas Biezemans <basbiezemans@users.noreply.github.com>
2025-01-11 21:09:17 +0000

    Refactor: use fromMaybe

--- src/Limit.hs
+++ src/Limit.hs
@@ -5,8 +5,9 @@ module Limit
     , safeValue
     ) where
 
+import Data.Maybe (fromMaybe)
 import Text.Read (readMaybe)
-import Utils (ensure)
+import Utils ((.:), ensure)
 
 newtype Limit = Limit
     { _limit :: Int
@@ -16,8 +17,7 @@ unLimit :: Limit -> Int
 unLimit = _limit
 
 limitOr :: Int -> Maybe Int -> Limit
-limitOr _  (Just x) = Limit x
-limitOr def Nothing = Limit def
+limitOr = Limit .: fromMaybe
 
 safeValue :: String -> Maybe Int
 safeValue s = readMaybe s >>= ensure (`elem` [8..12])
